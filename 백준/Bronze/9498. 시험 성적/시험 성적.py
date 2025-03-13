9fef289f0bed23f50e9c2d6a500cbd5e768e789c
Park Junsik <qkrwns1478@gmail.com>
2025-03-13 22:07:38 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+if 90 <= score:
+    grade = "A"
+elif 80 <= score:
+    grade = "B"
+elif 70 <= score:
+    grade = "C"
+elif 60 <= score:
+    grade = "D"
+else:
+    grade = "F"
+print(grade)
