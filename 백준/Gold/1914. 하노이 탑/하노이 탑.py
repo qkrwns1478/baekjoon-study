a34a22c3e3a43ee95724c2e8f6504b83221755cf
Park Junsik <qkrwns1478@gmail.com>
2025-03-14 15:55:22 +0900

    [Gold V] Title: 하노이 탑, Time: 860 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,10 @@
+def hanoi(n, s, e):
+    m = 6 - s - e
+    if n > 1: hanoi(n-1, s, m)
+    print(s, e)
+    if n > 1: hanoi(n-1, m, e)
+
+n = int(input())
+print(2**n - 1)
+if n <= 20:
+    hanoi(n, 1, 3)
