f3ddeee32839383f900e14cb2e443516d62d1d56
Park Junsik <qkrwns1478@gmail.com>
2025-03-22 21:22:59 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for i in range(1, n):
+    print('*' * i + ' ' * 2*(n-i) + '*' * i)
+for i in range(n, 0, -1):
+    print('*' * i + ' ' * 2*(n-i) + '*' * i)
