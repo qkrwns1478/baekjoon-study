8da9cf1f4c14b1c6190ae6c67ffd75d0bb365230
Park Junsik <qkrwns1478@gmail.com>
2025-03-25 16:19:12 +0900

    [Gold V] Title: 용액, Time: 172 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+pl, pr = 0, n-1
+answer = float("inf")
+A, B = None, None
+
+while pl < pr:
+    a, b = arr[pl], arr[pr]
+    if answer > abs(a+b):
+        answer = abs(a+b)
+        A, B = a, b
+    elif abs(a) > abs(b):
+        pl += 1
+    else:
+        pr -= 1
+
+if A > B: A, B = B, A
+print(A, B)
