d655e27441a703f295150002c6dfcfb2734e4342
Park Junsik <qkrwns1478@gmail.com>
2025-03-26 23:33:52 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a, b = 1, 0
+    for i in range(n):
+        a, b = b, a+b
+        #print(a, b)
+    print(a, b)
