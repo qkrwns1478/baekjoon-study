ec89964d54c921a6e3cd138883eb0e7d05a3bd24
Park Junsik <qkrwns1478@gmail.com>
2025-03-26 11:14:50 +0900

    [Gold IV] Title: 사냥꾼, Time: 152 ms, Memory: 110872 KB, Score: 60 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -10,16 +10,15 @@ for _ in range(n):
 
 sade.sort()
 animals.sort()
-#print(sade)
-#print(animals)
 
 hunted = [False] * n
 answer = 0
 for x in sade:
     for i in range(n):
-        a, b = animals[i][0], animals[i][1]
-        if abs(x-a) + b <= l and not hunted[i]:
-            answer += 1
-            hunted[i] = True
+        if not hunted[i]:
+            a, b = animals[i][0], animals[i][1]
+            if abs(x-a) + b <= l:
+                answer += 1
+                hunted[i] = True
 
 print(answer)
