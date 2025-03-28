1389f3bf21485d75618a0fd63dc4fc0da798a161
Park Junsik <qkrwns1478@gmail.com>
2025-03-28 11:46:34 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 584 ms, Memory: 40012 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,30 +3,29 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 def dfs(i):
-    if visited[i]:
-        return
-
     visited[i] = True
-    for j in adj[i]:
-        dfs(j)
+
+    for j in range(1, n+1):
+        if not visited[j] and adj[i][j]:
+            dfs(j)
 
 n, m = map(int, input().split())
-adj = dict()
-for i in range(1, n+1):
-    adj[i] = list()
+adj = [[0 for _ in range(n+1)] for _ in range(n+1)]
     
 for _ in range(m):
     u, v = map(int, input().split())
-    adj[u].append(v)
-    adj[v].append(u)
-    adj[u].sort()
-    adj[v].sort()
+    adj[u][v] = 1
+    adj[v][u] = 1
 
 answer = 0
 visited = [False] * (n+1)
 for i in range(1, n+1):
     if not visited[i]:
-        dfs(i)
-        answer += 1
+        if not adj[i]:
+            answer += 1
+            visited[i]
+        else:
+            dfs(i)
+            answer += 1
 
 print(answer)
