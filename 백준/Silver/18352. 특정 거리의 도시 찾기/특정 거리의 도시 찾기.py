089f17744b1c9e268097d83f1206610abc8b356f
Park Junsik <qkrwns1478@gmail.com>
2025-03-30 18:43:50 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1752 ms, Memory: 101228 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,40 +1,34 @@
 import sys
 input = sys.stdin.readline
-import heapq
+from collections import deque
 
 n, m, k, x = map(int, input().split())
+adj = [[] for _ in range(n+1)]
+visited = [-1] * (n+1)
 
-adj = dict()
-for i in range(1, n+1):
-    adj[i] = list()
-    
-for _ in range(m):
-    a, b = map(int, input().split())
-    adj[a].append((b, 1))
-
-dist = [float("inf")] * (n+1)
-
-def dij(s):
-    queue = list()
-    heapq.heappush(queue, (0, s))
-    dist[s] = 0
+def bfs(v):
+    queue = deque()
+    queue.append(v)
+    visited[v] += 1
 
     while queue:
-        now_dist, now = heapq.heappop(queue)
+        now = queue.popleft()
+        for i in adj[now]:
+            if visited[i] == -1:
+                visited[i] = visited[now] + 1
+                queue.append(i)
 
-        if dist[now] < now_dist:
-            continue
+for _ in range(m):
+    a, b = map(int, input().split())
+    adj[a].append(b)
 
-        for a, b in adj[now]:
-            if now_dist + b < dist[a]:
-                dist[a] = now_dist + b
-                heapq.heappush(queue, (now_dist + b, a))
-dij(x)
+bfs(x)
 
 answer = list()
-for i in range(1, n+1):
-    if dist[i] == k:
+for i in range(n+1):
+    if visited[i] == k:
         answer.append(i)
+
 if not answer:
     print(-1)
 else:
