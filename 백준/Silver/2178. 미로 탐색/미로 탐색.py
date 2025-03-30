f3608cb939e85aefa09b57f6e9d24f707b10e846
Park Junsik <qkrwns1478@gmail.com>
2025-03-30 16:43:32 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,53 +3,32 @@ input = sys.stdin.readline
 from collections import deque
 
 n, m = map(int, input().split())
-maze = [[0 for _ in range(m)] for _ in range(n)]
+adj = [[0 for _ in range(m)] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+
 for i in range(n):
-    s = input()
+    s = list(input())
     for j in range(m):
-        if s[j] == '1':
-            maze[i][j] = 1
+        adj[i][j] = int(s[j])
 
-visited = set()
-queue = deque()
-queue.append((0,0))
-#answer = 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-while queue:
-    v = queue.popleft()
-    #print(v, v in visited)
-    
-    if v not in visited:
-        visited.add(v)
-        x, y = v[0], v[1]
-        #flag = False
+def bfs(i, j):
+    queue = deque()
+    queue.append((i, j))
+    visited[i][j] = True
 
-        if x > 0 and maze[x-1][y] > 0 and (x-1, y) not in visited and (x-1, y) not in queue:
-            #answer += 1
-            #flag = True
-            maze[x-1][y] += maze[x][y]
-            queue.append((x-1, y))
-            
-        if x < n-1 and maze[x+1][y] > 0 and (x+1,y) not in visited and (x+1, y) not in queue:
-            #answer += 1
-            #flag = True
-            maze[x+1][y] += maze[x][y]
-            queue.append((x+1, y))
-            
-        if y > 0 and maze[x][y-1] > 0 and (x,y-1) not in visited and (x, y-1) not in queue:
-            #answer += 1
-            #flag = True
-            maze[x][y-1] += maze[x][y]
-            queue.append((x, y-1))
-            
-        if y < m-1 and maze[x][y+1] > 0 and (x,y+1) not in visited and (x, y+1) not in queue:
-            #answer += 1
-            #flag = True
-            maze[x][y+1] += maze[x][y]
-            queue.append((x, y+1))
-    
-    #if not flag:
-    #    answer -= 1
-    
-#print(answer)
-print(maze[n-1][m-1])
+    while queue:
+        x, y = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m:
+                if adj[nx][ny] != 0 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    adj[nx][ny] = adj[x][y] + 1
+                    queue.append((nx, ny))
+
+bfs(0, 0)
+print(adj[n-1][m-1])
