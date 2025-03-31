22e58b3f2542f7aa3e14aa0cea0889820560c577
Park Junsik <qkrwns1478@gmail.com>
2025-03-31 15:37:07 +0900

    [Gold IV] Title: 플로이드, Time: 160 ms, Memory: 111560 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-m = int(input().strip())
+n = int(input())
+m = int(input())
 dist = [[float("inf") for _ in range(n+1)] for _ in range(n+1)]
 for i in range(1, n+1):
     dist[i][i] = 0
 
-for i in range(m):
-    a, b, c = map(int, input().strip().split())
+for _ in range(m):
+    a, b, c = map(int, input().split())
     if dist[a][b] > c:
         dist[a][b] = c
 
