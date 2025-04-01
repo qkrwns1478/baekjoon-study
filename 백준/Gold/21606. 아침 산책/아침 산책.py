33505a01da9aa7cc9ec43a0aac0b03ef53a10c60
Park Junsik <qkrwns1478@gmail.com>
2025-04-01 22:00:19 +0900

    [Gold III] Title: 아침 산책, Time: 840 ms, Memory: 106868 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,41 +1,36 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
-
-def dfs(i):
-    global answer
-    
-    if visited[i]:
-        return
-
-    visited[i] = True
-    for j in adj[i][0]:
-        if not visited[j]:
-            if adj[j][1] == 0:
-                dfs(j)
-            else:
-                answer += 1
+from math import perm
 
 n = int(input())
-A = input() # 1 = 실내, 0 = 실외
-adj = dict()
-for i in range(1, n+1):
-    adj[i] = (list(), int(A[i-1]))
-    
-for _ in range(n-1):
-    u, v = map(int, input().split())
-    adj[u][0].append(v)
-    adj[v][0].append(u)
-
+adj = [[] for _ in range(n+1)]
+inout = list("0" + input().strip())
 answer = 0
-cnt = 0
-for i in range(1, n+1):
-    if adj[i][1] == 0:
-        continue
-    
-    visited = [False] * (n+1)
-    dfs(i)
-    answer += 1
-    cnt += 1
 
-print(answer - cnt)
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    adj[a].append(b)
+    adj[b].append(a)
+    if inout[a] == "1" and inout[b] == "1": # 연결된 두 노드가 실내-실내인 경우
+        answer += 2
+        
+def dfs(i):
+    cnt = 0
+    visited[i] = True
+    for j in adj[i]:
+        if not visited[j]:
+            if inout[j] == "1":
+                cnt += 1
+            else:
+                cnt += dfs(j)
+    return cnt      
+
+visited = [False] * (n+1)
+for i in range(1, n+1):
+    if inout[i] == "0":
+        if not visited[i]:
+            r = dfs(i)
+            answer += perm(r, 2) # 실외와 연결된 r개의 실내 중 2개를 선택하는 경우의 수
+
+print(answer)
