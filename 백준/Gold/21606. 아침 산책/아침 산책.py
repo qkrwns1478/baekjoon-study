5a5d8c6d2669d7b41ca16ec0c6bb5a7fd023c8c4
Park Junsik <qkrwns1478@gmail.com>
2025-04-01 21:42:59 +0900

    [Gold III] Title: 아침 산책, Time: 1452 ms, Memory: 96996 KB, Score: 73 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,39 +1,49 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**4)
-
-n = int(input())
-adj = dict()
-for i in range(1, n+1):
-    adj[i] = list()
-
-place = list("0" + input().strip())
-    
-for _ in range(n-1):
-    a, b = map(int, input().split())
-    adj[a].append(b)
-    adj[b].append(a)
+sys.setrecursionlimit(10**6)
 
 def dfs(i):
-    cnt = 0
+    global answer
+    
+    if visited[i]:
+        return
+
     visited[i] = True
-    for j in adj[i]:
+    for j in adj[i][0]:
         if not visited[j]:
-            if place[j] == "1": # 실내에 도착하면 종료
-                cnt += 1
+            if adj[j][1] == 0:
+                #print(f"{i} -> {j}")
+                dfs(j)
             else:
-                cnt += dfs(j)
-    return cnt      
+                #print(f"{j}에서 종료")
+                answer += 1
+
+n = int(input())
+A = input() # 1 = 실내, 0 = 실외
+adj = dict()
+for i in range(1, n+1):
+    adj[i] = (list(), int(A[i-1]))
+    
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    adj[u][0].append(v)
+    adj[v][0].append(u)
+    adj[u][0].sort()
+    adj[v][0].sort()
+
+#for i in range(1, n+1):
+#    print(f"{i} : {adj[i]}")
 
 answer = 0
+cnt = 0
 for i in range(1, n+1):
-    if place[i] == "0":
+    if adj[i][1] == 0:
         continue
+    
     visited = [False] * (n+1)
-    for j in range(i):
-        if place[j] == "1":
-            visited[j] = True
-    if not visited[i]:
-        answer += dfs(i)
+    #print(f"{i}에서 시작")
+    dfs(i)
+    answer += 1
+    cnt += 1
 
-print(answer*2)
+print(answer - cnt)
