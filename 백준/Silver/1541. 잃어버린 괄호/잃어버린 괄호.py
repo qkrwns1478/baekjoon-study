cf832d20ed8620d21be62ce4c945c9db43662366
Park Junsik <qkrwns1478@gmail.com>
2025-04-05 10:15:47 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,24 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-arr = list(input().strip().split("-"))
-#print(arr)
+s = input().strip().split('-')
 
-def sejun(s):
-    sum = 0
-    tmp = s.split("+")
-    for i in tmp:
-        sum += int(i)
-    return sum
-
-answer = 0
-for i in range(len(arr)):
-    tmp = sejun(arr[i])
-    #print(tmp)
-    if i == 0:
-        answer += tmp
+for i in range(len(s)):
+    if s[i].isdigit():
+        s[i] = int(s[i])
     else:
-        answer -= tmp
-    #print(answer)
+        tmp = list(map(int, s[i].split('+')))
+        s[i] = sum(tmp)
 
+answer = s[0]
+for i in range(1, len(s)):
+    answer -= s[i]
 print(answer)
