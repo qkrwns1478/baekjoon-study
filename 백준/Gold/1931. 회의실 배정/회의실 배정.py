2b11040a0ab38c63ab98e1523b8888497cf1c6c6
Park Junsik <qkrwns1478@gmail.com>
2025-04-05 10:44:06 +0900

    [Gold V] Title: 회의실 배정, Time: 252 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -2,19 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [[0] * 2 for _ in range(n)]
-for i in range(n):
-    s, e = map(int, input().split())
-    arr[i][1] = s
-    arr[i][0] = e
+arr = list(tuple(map(int, input().split())) for _ in range(n))
+arr.sort(key=lambda x: (x[1], x[0]))
+#print(arr)
 
-arr.sort()
 answer = 0
 end = -1
-
 for i in range(n):
-    if arr[i][1] >= end:
-        end = arr[i][0]
+    s, e = arr[i]
+    if s >= end:
+        end = e
         answer += 1
-
 print(answer)
