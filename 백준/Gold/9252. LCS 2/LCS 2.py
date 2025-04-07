aeb3be25a6dbb6d1b567e0b051d1aa4a9c15090b
Park Junsik <qkrwns1478@gmail.com>
2025-04-07 14:09:06 +0900

    [Gold IV] Title: LCS 2, Time: 124 ms, Memory: 130892 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,19 +1,19 @@
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**4)
 
-A = input().strip()
-B = input().strip()
+A = input()
+B = input()
 a, b = len(A), len(B)
-answer = list()
 
-dp = [[0] * (b+1) for _ in range(a+1)]
+dp = [[0] * (b+1) for _  in range(a+1)]
 for i in range(1, a+1):
     for j in range(1, b+1):
         if A[i-1] == B[j-1]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            
+print(dp[-1][-1])
 
 def LCS(i, j):
     if i == 0 or j == 0:
@@ -27,8 +27,7 @@ def LCS(i, j):
         else:
             LCS(i, j-1)
 
-print(dp[a][b])
+answer = list()
 LCS(a, b)
-if answer:
-    answer.reverse()
-    print(*answer, sep="")
+answer.reverse()
+print(''.join(answer))
