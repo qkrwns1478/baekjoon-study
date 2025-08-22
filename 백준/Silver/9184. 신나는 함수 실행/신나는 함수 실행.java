a8ec2c924ff927c2c8f4218d3254a3b5d51d2440
Park Junsik <qkrwns1478@gmail.com>
2025-08-22 16:23:01 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1852 ms, Memory: 83484 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int[][][] dp = new int[21][21][21];
+
+    public static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        } else if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        } else if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        } else if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+            return dp[a][b][c];
+        } else {
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+            return dp[a][b][c];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+        }
+    }
+}
