bddb187bb15d1fad4ba64be509ba7b92f9d89435
Park Junsik <qkrwns1478@gmail.com>
2025-09-20 21:38:43 +0900

    [Bronze IV] Title: 모음의 개수, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int answer = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+        scanner.close();
+    }
+}
