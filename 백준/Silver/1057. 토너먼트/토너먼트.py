3a88a9227c9408311fdafb5cb08d93ebe616f6a1
Park Junsik <qkrwns1478@gmail.com>
2025-09-24 18:04:29 +0900

    [Silver IV] Title: 토너먼트, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.py
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -1,21 +1,10 @@
-def promote(x):
-    if x % 2 == 0:
-        return x // 2
-    else:
-        return (x+1) // 2
+from math import ceil
 
-n, kim, lee = map(int, input().split())
-a, b = min(kim, lee), max(kim, lee)
-ans = 1
-while True:
-    if not (b-a == 1 and a % 2 == 1):
-        ans += 1
-        a = promote(a)
-        b = promote(b)
-        if n % 2 == 0:
-            n //= 2
-        else:
-            n = n//2 + 1
-    else:
-        print(ans)
-        break
+n, k, l = map(int, input().split())
+a, b = min(k, l), max(k, l)
+ans = 0
+while a != b:
+    ans += 1
+    a = ceil(a/2)
+    b = ceil(b/2)
+print(ans)
