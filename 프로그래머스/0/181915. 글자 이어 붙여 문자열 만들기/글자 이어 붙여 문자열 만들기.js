401b8dd0afc0cf612e856c12aff34e56421a340c
Park Junsik <qkrwns1478@gmail.com>
2025-09-27 15:05:00 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,6 @@
+const solution = (my_string, index_list) => {
+    var answer = '';
+    for (let i = 0; i < index_list.length; i++)
+        answer += my_string[index_list[i]];
+    return answer;
+};
