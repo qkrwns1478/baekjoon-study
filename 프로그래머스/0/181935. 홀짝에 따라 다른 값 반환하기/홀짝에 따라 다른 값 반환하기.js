9ab951635469664dda6e91e7883e087778f12c72
Park Junsik <qkrwns1478@gmail.com>
2025-09-27 13:09:54 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,13 @@
+const solution = (n) => {
+    let answer = 0;
+    if (n % 2 == 1) {
+        for (let i = 1; i <= n; i+=2) {
+            answer += i;
+        }
+    } else {
+        for (let i = 2; i <= n; i+=2) {
+            answer += i * i;
+        }
+    }
+    return answer;
+}
