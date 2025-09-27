214c17784d394b930b0e953e78c3c0e720117c6a
Park Junsik <qkrwns1478@gmail.com>
2025-09-27 12:14:57 +0900

    [level 0] Title: 문자열 돌리기, Time: 285.38 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    for (let i = 0; i < line.length; i++) {
+        console.log(line[i]);
+    }
+});
