4b1da21ddda394ba3b939fead410051b976c2b4c
Park Junsik <qkrwns1478@gmail.com>
2025-11-17 13:51:42 +0900

    [D1] Title: 중간값 찾기, Time: 5 ms, Memory: 404 KB -BaekjoonHub

---
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+    	cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    cout << arr[N/2] << endl;
+	return 0;
+}
