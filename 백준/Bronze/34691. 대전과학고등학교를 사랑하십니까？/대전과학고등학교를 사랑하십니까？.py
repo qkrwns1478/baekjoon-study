27f97ffd6a917e588df96d894321352a4de7bbd9
Park Junsik <qkrwns1478@gmail.com>
2025-12-20 17:39:05 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,10 @@
+while True:
+    s = input()
+    if s == 'end':
+        break
+    elif s == 'animal':
+        print('Panthera tigris')
+    elif s == 'tree':
+        print('Pinus densiflora')
+    else:
+        print('Forsythia koreana')
