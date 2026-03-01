fa69e3ee152c4b21c035ab13b2260512d1b55d3f
Park Junsik <qkrwns1478@gmail.com>
2026-03-01 20:25:33 +0900

    [Silver IV] Title: 문자열 줄이기, Time: 416 ms, Memory: 163536 KB -BaekjoonHub

--- 백준/Silver/32627. 문자열 줄이기/문자열 줄이기.py
+++ 백준/Silver/32627. 문자열 줄이기/문자열 줄이기.py
@@ -1,10 +1,1 @@
-import sys
-input=sys.stdin.readline
-N,M=map(int,input().split())
-S=input().strip()
-A=[(S[i],i) for i in range(N)]
-A.sort(key=lambda x:(x[0],x[1]))
-A=A[M:]
-A.sort(key=lambda x:x[1])
-for i in range(len(A)):
-    print(A[i][0],end='')
+I=input;N,M=map(int,I().split());S=I();A=[(S[i],i) for i in range(N)];A.sort(key=lambda x:(x[0],x[1]));A=A[M:];A.sort(key=lambda x:x[1]);print(''.join(x[0] for x in A))
