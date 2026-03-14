c12281e569c6fe86f88d46508577205a3fa0719b
Park Junsik <qkrwns1478@gmail.com>
2026-03-14 15:22:15 +0900

    [Bronze IV] Title: Quadrants, Time: 192 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            if (x == 0 || y == 0) {
+                System.out.println("AXIS");
+                if (x == 0 && y == 0) break;
+            }
+            else if (x > 0 && y > 0) System.out.println("Q1");
+            else if (x < 0 && y > 0) System.out.println("Q2");
+            else if (x < 0) System.out.println("Q3");
+            else System.out.println("Q4");
+        }
+        scanner.close();
+    }
+}
