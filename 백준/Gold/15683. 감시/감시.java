210fbfd11352238c9d304c60878942f110cb534f
Park Junsik <qkrwns1478@gmail.com>
2026-03-19 21:07:42 +0900

    [Gold III] Title: 감시, Time: 320 ms, Memory: 65184 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     private static final int[] dx = {0, 0, -1, 1};
     private static final int[] dy = {1, -1, 0, 0};
-    private static int N, M, L;
+    private static int N, M;
     private static int ans = Integer.MAX_VALUE;
     private static ArrayList<int[]> cameras;
 
@@ -16,7 +16,7 @@ public class Main {
     }
 
     private static void dfs(int[][] arr, int level) {
-        if (level == L) {
+        if (level == cameras.toArray().length) {
             int cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
@@ -27,77 +27,30 @@ public class Main {
             return;
         }
 
+        int[][] dd;
         int[] cur = cameras.get(level);
         int type = cur[0], x = cur[1], y = cur[2];
         if (type == 1) {
-            for (int i = 0; i < 4; i++) {
-                int[][] newArr = copyArray(arr);
-                int nx = x + dx[i], ny = y + dy[i];
-                while (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (newArr[nx][ny] == 6) break;
-                    newArr[nx][ny] = -1;
-                    nx += dx[i];
-                    ny += dy[i];
-                }
-                dfs(newArr, level+1);
-            }
+            dd = new int[][]{{0}, {1}, {2}, {3}};
         } else if (type == 2) {
-            int[][] dd = {{0, 1}, {2, 3}};
-            for (int i = 0; i < 2; i++) {
-                int[][] newArr = copyArray(arr);
-                for (int j = 0; j < 2; j++) {
-                    int nx = x + dx[dd[i][j]];
-                    int ny = y + dy[dd[i][j]];
-                    while (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                        if (newArr[nx][ny] == 6) break;
-                        newArr[nx][ny] = -1;
-                        nx += dx[dd[i][j]];
-                        ny += dy[dd[i][j]];
-                    }
-                }
-                dfs(newArr, level+1);
-            }
+            dd = new int[][]{{0, 1}, {2, 3}};
         } else if (type == 3) {
-            int[][] dd = {{0, 2}, {0, 3}, {1, 3}, {1, 2}};
-            for (int i = 0; i < 4; i++) {
-                int[][] newArr = copyArray(arr);
-                for (int j = 0; j < 2; j++) {
-                    int nx = x + dx[dd[i][j]];
-                    int ny = y + dy[dd[i][j]];
-                    while (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                        if (newArr[nx][ny] == 6) break;
-                        newArr[nx][ny] = -1;
-                        nx += dx[dd[i][j]];
-                        ny += dy[dd[i][j]];
-                    }
-                }
-                dfs(newArr, level+1);
-            }
+            dd = new int[][]{{0, 2}, {0, 3}, {1, 3}, {1, 2}};
         } else if (type == 4) {
-            int[][] dd = {{0, 1, 2}, {0, 2, 3}, {0, 1, 3}, {1, 2, 3}};
-            for (int i = 0; i < 4; i++) {
-                int[][] newArr = copyArray(arr);
-                for (int j = 0; j < 3; j++) {
-                    int nx = x + dx[dd[i][j]];
-                    int ny = y + dy[dd[i][j]];
-                    while (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                        if (newArr[nx][ny] == 6) break;
-                        newArr[nx][ny] = -1;
-                        nx += dx[dd[i][j]];
-                        ny += dy[dd[i][j]];
-                    }
-                }
-                dfs(newArr, level+1);
-            }
+            dd = new int[][]{{0, 1, 2}, {0, 2, 3}, {0, 1, 3}, {1, 2, 3}};
         } else {
+            dd = new int[][]{{0, 1, 2, 3}};
+        }
+        for (int i = 0; i < dd.length; i++) {
             int[][] newArr = copyArray(arr);
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
+            for (int j = 0; j < dd[i].length; j++) {
+                int nx = x + dx[dd[i][j]];
+                int ny = y + dy[dd[i][j]];
                 while (0 <= nx && nx < N && 0 <= ny && ny < M) {
                     if (newArr[nx][ny] == 6) break;
                     newArr[nx][ny] = -1;
-                    nx += dx[i];
-                    ny += dy[i];
+                    nx += dx[dd[i][j]];
+                    ny += dy[dd[i][j]];
                 }
             }
             dfs(newArr, level+1);
@@ -118,7 +71,6 @@ public class Main {
                     cameras.add(new int[]{t, i, j});
             }
         }
-        L = cameras.toArray().length;
 
         dfs(A, 0);
         System.out.println(ans);
