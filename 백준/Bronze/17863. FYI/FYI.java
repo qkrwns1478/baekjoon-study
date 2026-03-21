e158f504afde0e32abe864e3322390080bba795a
Park Junsik <qkrwns1478@gmail.com>
2026-03-21 20:39:13 +0900

    [Bronze IV] Title: FYI, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input.startsWith("555") ? "YES" : "NO");
+        br.close();
+    }
+}
