81503c0d1fef2243cab75cb6ac83b2f3f3596beb
Park Junsik <qkrwns1478@gmail.com>
2026-03-29 14:49:31 +0900

    [Gold V] Title: 내일 할거야, Time: 2304 ms, Memory: 339640 KB -BaekjoonHub

---
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] A = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            String[] inputs = br.readLine().split(" ");
+            for (int j = 0; j < 2; j++) {
+                A[i][j] = Integer.parseInt(inputs[j]);
+            }
+        }
+        Arrays.sort(A, (a, b) -> b[1] - a[1]);
+
+        int ans = A[0][1];
+        for (int i = 0; i < N; i++) {
+            int S = A[i][0];
+            int E = A[i][1];
+            if (ans >= E) ans = E - S;
+            else ans -= S;
+        }
+        bw.write(String.valueOf(ans));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
