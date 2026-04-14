136432b9f66fa89e6b622593baf10236b61e723c
Park Junsik <qkrwns1478@gmail.com>
2026-04-14 19:17:37 +0900

    [Bronze II] Title: SPC에 가는 길, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/32626. SPC에 가는 길/SPC에 가는 길.java
+++ 백준/Bronze/32626. SPC에 가는 길/SPC에 가는 길.java
@@ -15,20 +15,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int[] S = getArray(2);
-        int sx = S[0], sy = S[1];
         int[] E = getArray(2);
-        int ex = E[0], ey = E[1];
         int[] P = getArray(2);
+        int sx = S[0], sy = S[1];
+        int ex = E[0], ey = E[1];
         int px = P[0], py = P[1];
 
-        int ans;
         boolean con1 = (py > sy && py < ey) || (py > ey && py < sy);
         boolean con2 = (px > sx && px < ex) || (px > ex && px < sx);
         boolean con3 = px == sx && con1;
         boolean con4 = py == sy && con2;
-        if (sx == ex) ans = con3 ? 2 : 0;
-        else if (sy == ey) ans = con4 ? 2 : 0;
-        else ans = 1;
+        int ans = sx == ex ? (con3 ? 2 : 0) : sy == ey ? (con4 ? 2 : 0) : 1;
         bw.write(String.valueOf(ans));
 
         bw.flush();
