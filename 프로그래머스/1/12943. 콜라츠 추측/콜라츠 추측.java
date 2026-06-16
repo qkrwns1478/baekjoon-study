b204af435a04e699f0648675664fa75f2effb353
Park Junsik <qkrwns1478@gmail.com>
2026-06-16 10:05:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        while (num != 1 && answer < 500) {
+            if (num % 2 == 0) num /= 2;
+            else num = num * 3 + 1;
+            answer++;
+        }
+        
+        return answer >= 500 ? -1 : answer;
+    }
+}
