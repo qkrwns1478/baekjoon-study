1b1736cf6933f3635eba80ac82bc9983b219b59c
Park Junsik <qkrwns1478@gmail.com>
2026-09-10 17:48:38 +0900

    [Passed] 사각형 채우기 3 (140ms, 10MB)

---
+++ 코드트리/trail4/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        long[] dp = new long[n+1];
+        dp[0] = 1; dp[1] = 2;
+        int MOD = 1_000_000_007;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % MOD;
+            for (int j = i-3; j >= 0; j--) dp[i] = (dp[i] + dp[j] * 2) % MOD;
+        }
+        System.out.println(dp[n]);
+    }
+}
