fc611d0fda789139498f61f4d6478d12dff140cc
Park Junsik <qkrwns1478@gmail.com>
2026-09-11 09:56:25 +0900

    [Passed] 사각형 채우기 2 (127ms, 10MB)

---
+++ 코드트리/trail4/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] * 2 + (i % 2 == 0 ? 1 : -1)) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
