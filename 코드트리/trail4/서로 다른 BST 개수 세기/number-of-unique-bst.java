bac78627d6f3dbcec66d941dbf314866142986d2
Park Junsik <qkrwns1478@gmail.com>
2026-09-11 09:44:06 +0900

    [Passed] 서로 다른 BST 개수 세기 (153ms, 10MB)

---
+++ 코드트리/trail4/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        int[] dp = new int[20];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
