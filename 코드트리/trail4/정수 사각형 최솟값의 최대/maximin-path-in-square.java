b8799055aa4895fa86a719863f5aee7f639dddc6
Park Junsik <qkrwns1478@gmail.com>
2026-09-11 11:05:11 +0900

    [Passed] 정수 사각형 최솟값의 최대 (135ms, 10MB)

---
+++ 코드트리/trail4/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+        int[][] dp = new int[n][n];
+        dp[0][0] = matrix[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i-1][0], matrix[i][0]);
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = Math.min(dp[0][j-1], matrix[0][j]);
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                int tmp = Math.max(dp[i-1][j], dp[i][j-1]);
+                dp[i][j] = Math.min(matrix[i][j], tmp);
+            }
+        }
+        System.out.println(dp[n-1][n-1]);
+    }
+}
